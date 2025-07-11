451dd1dea776b1ec487f5df7a7884e5fd3477197
yjieuny <asd654@hanmail.net>
2025-07-11 18:33:38 +0900

    fix: Display created_at and updated_at using Korean Standard Time(KST)

--- api/models/postDao.js
+++ api/models/postDao.js
@@ -1,21 +1,37 @@
 const { pool } = require('./database');
 
+const dayjs = require('dayjs');
+const utc = require('dayjs/plugin/utc');
+const timezone = require('dayjs/plugin/timezone');
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+const getKstDate = () => dayjs().tz('Asia/Seoul').format('YYYY-MM-DD HH:mm:ss');
+
 const createPost = async ({ title, content, user_id }) => {
   try {
-    const query = `
-      INSERT INTO posts (title, content, user_id, created_at, updated_at)
-      VALUES (?, ?, ?, NOW(), NOW())
-    `;
+    const kstDate = getKstDate();
 
-    const [result] = await pool.execute(query, [title, content, user_id]);
+    const query = `
+        INSERT INTO posts (title, content, user_id, created_at, updated_at)
+        VALUES (?, ?, ?, ?, ?)
+      `;
+
+    const [result] = await pool.execute(query, [
+      title,
+      content,
+      user_id,
+      kstDate,
+      kstDate,
+    ]);
 
     return {
       id: result.insertId,
       title,
       content,
       user_id,
-      created_at: new Date(),
-      updated_at: new Date(),
+      created_at: kstDate,
+      updated_at: kstDate,
     };
   } catch (err) {
     console.error('Database error:', err);
@@ -26,11 +42,11 @@ const createPost = async ({ title, content, user_id }) => {
 const findAllPost = async () => {
   try {
     const query = `
-    SELECT p.id, p.title, p.content, p.created_at, p.updated_at, p.user_id, u.nickname AS user_nickname
-    FROM posts p
-    JOIN users u ON p.user_id = u.id
-    ORDER BY p.created_at DESC
-    `;
+      SELECT p.id, p.title, p.content, p.created_at, p.updated_at, p.user_id, u.nickname AS user_nickname
+      FROM posts p
+      JOIN users u ON p.user_id = u.id
+      ORDER BY p.created_at DESC
+      `;
 
     const [rows] = await pool.execute(query);
     return rows;
@@ -41,19 +57,21 @@ const findAllPost = async () => {
 };
 
 const updatePost = async (id, { title, content }) => {
+  const kstDate = getKstDate();
+
   const query = `
-  UPDATE posts
-  SET title = ?, content = ?, updated_at = NOW()
-  WHERE id = ?
-  `;
-  const [result] = await pool.execute(query, [title, content, id]);
+    UPDATE posts
+    SET title = ?, content = ?, updated_at = ?
+    WHERE id = ?
+    `;
+  const [result] = await pool.execute(query, [title, content, , kstDate, id]);
   return result;
 };
 
 const deletePost = async (id) => {
   const query = `
-  DELETE FROM posts WHERE id = ?
-  `;
+    DELETE FROM posts WHERE id = ?
+    `;
   const [result] = await pool.execute(query, [id]);
   return result;
 };
